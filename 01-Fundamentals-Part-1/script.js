40ff53584443446a8fd05a787c9f27db71aaac96
JobVillagran <jobvillagran@gmail.com>
2024-01-09 08:53:29 -0600

    mod[let, const and var]

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,7 +1,7 @@
-let js = "boring";
-if (js === "amazing") alert("JavaScriot is FUN!");
+// let js = "boring";
+// if (js === "amazing") alert("JavaScriot is FUN!");
 
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
 /****** Values and Variables ******/
 
@@ -20,22 +20,21 @@ console.log(myFirstJob, myCurrentJob);*/
 
 
 
-
 /****** Primite Data types ******/
 
 // JavaScript has dynamic typing: We do not have to manually define the data type of the value stored in a variable. Instead, data types are determined automatically.
 
 //Number
-let age = 23;
+// let age = 23;
 
-//String
-let secondName = "Emanuel";
+// //String
+// let secondName = "Emanuel";
 
-//Boolean
-let fullAge = true;
+// //Boolean
+// let fullAge = true;
 
-//Undefined
-let children;
+// //Undefined
+// let children;
 
 //Null
 
@@ -48,26 +47,36 @@ let children;
 
 //Examples
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
-console.log(typeof true);
-console.log(typeof javascriptIsFun + " " + "javascriptIsFun");
-console.log(typeof 23);
-console.log(typeof 'Job');
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun + " " + "javascriptIsFun");
+// console.log(typeof 23);
+// console.log(typeof 'Job');
 
 // Le cambiamos a la variable su contenido. Con Let asignamos y
 // sin Let asignamos un nuevo valor. 
-javascriptIsFun = "YES!";
-console.log(typeof javascriptIsFun + " " + "javascriptIsFun");
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun + " " + "javascriptIsFun");
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1998;
-console.log(year);
-console.log(typeof year);
+// year = 1998;
+// console.log(year);
+// console.log(typeof year);
 
 // This examples is a bug but from JS cuz is not recognizing properly
 // the data type.
-console.log(typeof null);
+//console.log(typeof null);
+
+
+
+/****** let, const and var ******/
+
+let age = 31;
+//Mutate the variable
+age = 21;
+
+console.log(age);
