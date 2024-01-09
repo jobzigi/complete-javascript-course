3d253ba334a1d5fc7da5f0edd0e6fdc538f5a7d5
JobVillagran <jobvillagran@gmail.com>
2024-01-09 06:59:13 -0600

    mod[Values/Variables]

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -2,3 +2,18 @@ let js = "boring";
 if (js === "amazing") alert("JavaScriot is FUN!");
 
 console.log(40 + 8 + 23 - 10);
+
+//Values and Variables
+
+let fisrtName = "Larry";
+let PI = 3.14159;
+let myFirstJob = "Grocer";
+let myCurrentJob = "Software Engineer";
+
+console.log(fisrtName);
+console.log(fisrtName);
+console.log(fisrtName);
+console.log(PI);
+
+
+console.log(myFirstJob, myCurrentJob);
