ebc883b76891ce755a4107ec7b4979aeaac65355
JobVillagran <jobvillagran@gmail.com>
2024-01-15 08:00:48 -0600

    mod[course_structure]

---
+++ 01-Fundamentals-Part-1/02_values_variables.js
@@ -0,0 +1,14 @@
+/** **** Values and Variables ******/
+
+let fisrtName = "Larry";
+let PI = 3.14159;
+let myFirstJob = "Grocer";
+let myCurrentJob = "Software Engineer";
+
+console.log(fisrtName);
+console.log(fisrtName);
+çç
+console.log(PI);
+
+
+console.log(myFirstJob, myCurrentJob);
